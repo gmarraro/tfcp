f4d091758c570f66cb6832599722a65bea87375f
gabmarr <marrarogaby@gmail.com>
2019-11-16 16:01:13 -0500

    Revert "moved to src folder, added fonts"
    This reverts commit af478ab78ebca35699c7c177611372243fd97e70.

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,5 +29,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        custom: {
+          families: ["Martin, GT America, GT America Compressed"],
+          urls: ["/fonts/fonts.css"],
+        },
+      },
+    },
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,7 @@ const IndexPage = () => (
       <Image />
     </div>
     <Button text={"Donate"} />
+    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
