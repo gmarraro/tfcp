4037e3385192459caf9400bf13a7ccf8126fbf81
Kaitlin Gu <kaitlin.gu@gmail.com>
2019-11-21 01:20:03 -0500

    Updating image component to use png

--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "ourStory.jpg" }) {
+      placeholderImage: file(relativePath: { eq: "ourStory.png" }) {
         childImageSharp {
           fluid(maxWidth: 7682) {
             ...GatsbyImageSharpFluid
