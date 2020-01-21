3f399a8f9b25e08238f3126eb90b157e9c12c190
gabmarr <marrarogaby@gmail.com>
2020-01-20 19:33:01 -0500

    test contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,12 @@
 import React from "react"
 
 import SEO from "../components/seo"
+import ContactForm from "components/contactForm/contactForm"
 
 const ContactPage = () => (
   <div>
     <SEO title="Contact Us" />
+    <ContactForm />
   </div>
 )
 
