1b1d84da2c5f9d48a61d9627d616ce154017a523
gabmarr <marrarogaby@gmail.com>
2020-02-27 21:06:38 -0500

    test form

--- src/components/joinTheMovement/joinTheMovement.js
+++ src/components/joinTheMovement/joinTheMovement.js
@@ -59,6 +59,29 @@ const JoinTheMovement = () => {
         consider donating today.
       </p>
       <div className="ctasContainer">
+
+      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="bot-field" />
+        <input type="hidden" name="form-name" value="contact" />
+        <div className="field half first">
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" id="name" />
+        </div>
+        <div className="field half">
+            <label htmlFor="email">Email</label>
+            <input type="text" name="email" id="email" />
+        </div>
+        <div className="field">
+            <label htmlFor="message">Message</label>
+            <textarea name="message" id="message" rows="6"></textarea>
+        </div>
+        <ul className="actions">
+            <li><input type="submit" value="Send Message" className="special" /></li>
+            <li><input type="reset" value="Clear" /></li>
+        </ul>
+    </form>
+
+
         <div className={`border-top ${signingUp ? "no-padding" : ""}`} onClick={() => setSigningUp(true)}>
           {signingUp ? (
             <form
